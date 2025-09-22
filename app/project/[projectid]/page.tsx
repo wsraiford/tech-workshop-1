09685e47aeaef978d32be38d40c9909ec283fc09
wsraiford <wsraiford@icloud.com>
2025-09-22 19:46:30 -0400

    set static pages for github

--- app/project/[projectid]/page.tsx
+++ app/project/[projectid]/page.tsx
@@ -2,4 +2,15 @@ type Params = {
     projectId: string;
 }
 
-const ProjectName
+
+export async function generateStaticParams(): Promise<Params[]> {
+    return ["project1", "project2", "project3"].map(p => ({ projectId: p }));
+}
+
+const ProjectNamePage = ({ params }: { params: Params }) => {
+    return (
+        <p>This page features project {params.projectId}</p>
+    );
+};
+
+export default ProjectNamePage;
