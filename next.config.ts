896c8007a607c29c1b10cf6492734a5d63a447b3
wsraiford <wsraiford@icloud.com>
2025-09-22 19:16:32 -0400

    added github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+
+const repo = "ktp-workshop-1";
 const nextConfig: NextConfig = {
   /* config options here */
-};
+    output: "export",
+    basePath: `/${repo}/`,
+    assetPrefix: `/${repo}/`,
 
+};
+const nextConfig: NextConfig = {
+
+    output: "export",
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`,
+
+
+
+
+};
 export default nextConfig;
