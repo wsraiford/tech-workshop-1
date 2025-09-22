7a14141385759efd0238984d17c888421f83433c
wsraiford <wsraiford@icloud.com>
2025-09-22 19:42:09 -0400

    changed to deploy.yml

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
-import Image from "next/image";
-import {generateAllParamCombinations} from "next/dist/build/static-paths/app";
 import Greeting from "@/components/Greeting";
 
 export default function Home() {
-  return (
-      <section className="bg-teal-100 min-h-screen flex flex-col items-center">
-          <h1 className ="txt-3xl font bold text-blue-600 pt-4">Hello World</h1>
-          <button className="flex flex-wrap items-center justify-center w-full">
-              <button className="mt-4rounded bg-black px-4 text-white cursor-pointer hover: bg-gray-700"> Click Me</button>
-              <Greeting name="KTP"/>
-           </button>
-
-      </section>
-
-
-
-  );
+    return (
+        <section className="bg-teal-100 min-h-screen flex flex-col items-center">
+            <h1 className="text-3xl font-bold text-blue-600 text-center pt-4">Hello World</h1>
+            <button className="mt-4 rounded bg-black px-4 py-2 text-white cursor-pointer hover:bg-gray-700">Click me
+            </button>
+            <Greeting name="KTP" />
+            <Greeting name="World" />
+            <Greeting name="William" />
+        </section>
+    );
 }
